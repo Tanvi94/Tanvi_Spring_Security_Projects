bb9c8af1b318bfe22061c192a5435b619574fae1
Tanvi94 <madhekartanvi9@gmail.com>
2023-05-28 21:00:13 +0200

    Custom Access Denied Page

--- spring-security-demo-07-user-roles/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
+++ spring-security-demo-07-user-roles/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -12,5 +12,13 @@ public class LoginController {
 		return "fancy-login"; //map to jsp page
 		
 	}
+	
+	//add request mapping for /access-denied
+	@GetMapping("/access-denied")
+	public String showAccessDenied() {
+		
+		return "access-denied"; //map to jsp page
+		
+	}
 
 }
